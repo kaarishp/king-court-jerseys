f8908625a0019438cc6f69397b2179c0926169ef
kaarishp <kingkaarish@gmail.com>
2024-03-14 18:13:41 -0400

    shop styling design

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -3,6 +3,16 @@ import React from 'react'
 export const Product = (props) => {
   const { id, productName, price, productImage } = props.data;
   return (
-    <div>product</div>
+    <div className='product'>
+        <img src={productImage}/>
+        <div className='description'>
+            <p>
+                <b>{productName}</b>
+            </p>
+            <p>
+                <b>${price}</b>
+            </p>
+        </div>
+    </div>
   )
 }
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { PRODUCTS } from "../../products";
 import { Product } from "./product";
-// import "./shop.css";
+import "./shop.css";
 
 export const Shop = () => {
   return (
     <div className="shop">
       <div className="shopTitle">
-        <h1>PedroTech Shop</h1>
+        <h1>TechKingdom Shop</h1>
       </div>
 
       <div className="products">
