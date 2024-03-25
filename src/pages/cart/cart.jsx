09d3ddf5ceb8bf299e25d43fdfb6128265bdcced
kaarishp <kingkaarish@gmail.com>
2024-03-25 19:53:17 -0400

    styling bug issues fixed

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -3,6 +3,7 @@ import { ShopContext } from "../../context/shop-context";
 import { PRODUCTS } from "../../products";
 import { CartItem } from "./cart-item";
 import { useNavigate } from "react-router-dom";
+import "./cart.css";
 
 export const Cart = () => {
   const { cartItems, getTotalCartAmount, checkout } = useContext(ShopContext);
